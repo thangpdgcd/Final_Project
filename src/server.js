149c45cf9b25cf473813750f5b61ce63aa7aa4e8
Phan Đình Thắng <106921106+thangpdgcd@users.noreply.github.com>
2025-07-22 02:04:04 +0700

    fix-file server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import configViewEngine from "../src/config/viewEngine.js";
-import initRoutes from "../src/routes/index.js"; // nếu có định tuyến
-import initUserRoutes from "./routes/usersRoutes.js"; // nếu có định tuyến người dùng
-import initProductsRoutes from "./routes/productsRoutes.js"; // nếu có định tuyến sản phẩm
-import initCategoriesRoutes from "../src/routes/categoriesRoutes.js"; // nếu có định tuyến danh mục
+import initRoutes from "../src/routes/index.js";
+import initUserRoutes from "./routes/usersRoutes.js";
+import initProductsRoutes from "./routes/productsRoutes.js";
+import initCategoriesRoutes from "../src/routes/categoriesRoutes.js";
+import initOrdersRoutes from "./routes/ordersRoutes.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -20,7 +21,7 @@ initRoutes(app); // nếu có
 initUserRoutes(app); // nếu có
 initProductsRoutes(app); // nếu có định tuyến sản phẩm
 initCategoriesRoutes(app); // nếu có định tuyến danh mục
-
+initOrdersRoutes(app); // nếu có định tuyến đơn hàng
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
