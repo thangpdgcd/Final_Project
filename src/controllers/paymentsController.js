1dbbe10afc21ddbb7c04a3863e047e09d0eb9912
Phan Đình Thắng <106921106+thangpdgcd@users.noreply.github.com>
2025-07-25 15:42:23 +0700

    creat-paymentsController.js

---
+++ src/controllers/paymentsController.js
@@ -0,0 +1,60 @@
+import paymentService from "../service/paymentsService.js";
+
+let createPayments = async (req, res) => {
+  try {
+    let data = req.body;
+    let payment = await paymentService.createPayments(data);
+    return res.json(payment); // trả về JSON nếu dùng API
+    // nếu dùng EJS
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+let getAllPayments = async (req, res) => {
+  try {
+    let payments = await paymentService.getAllPayments();
+    return res.status(200).json(payments);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+let getPaymentsById = async (req, res) => {
+  try {
+    let id = req.params.id;
+    let payment = await paymentService.getPaymentsById(id);
+    return res.status(200).json(payment);
+  } catch (error) {
+    return res.status(404).json({ message: error.message });
+  }
+};
+
+let updatePayments = async (req, res) => {
+  try {
+    let id = req.params.id;
+    let data = req.body;
+    let payment = await paymentService.updatePayments(id, data);
+    return res.status(200).json(payment);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+let deletePayments = async (req, res) => {
+  try {
+    let id = req.params.id;
+    let result = await paymentService.deletePayments(id);
+    return res.status(200).json(result);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+export default {
+  createPayments,
+  getAllPayments,
+  getPaymentsById,
+  updatePayments,
+  deletePayments,
+};
