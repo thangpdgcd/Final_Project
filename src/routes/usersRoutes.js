0459aea0cb2f7740ce9e979e1c1be2f4435645b3
Phan Đình Thắng <106921106+thangpdgcd@users.noreply.github.com>
2025-09-02 17:51:38 +0700

    create file usersRoutes

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -4,19 +4,21 @@ import userController from "../../src/controllers/usersController.js"; // Import
 const router = express.Router();
 
 // GET all users
-router.get("/users", userController.getAllConUsers);
+router.get("/users", userController.getAllUsers);
 
 // GET user by ID
 router.get("/users/:id", userController.getOneUsers);
 
 // POST create user
-router.post("/create-users", userController.createUsers);
+router.post("/create-users", userController.createAdmin);
 
 // PUT update user
 router.put("/users/:id", userController.updateUsers);
 
 // DELETE user
 router.delete("/users/:id", userController.deleteUsers);
+
+// Search users
 
 // Attach all user routes under /api
 const initUserRoutes = (app) => {
