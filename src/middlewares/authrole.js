7e8363b5f2633562a3ff3820867cfc9884049d57
Phan Đình Thắng <106921106+thangpdgcd@users.noreply.github.com>
2025-09-02 17:42:56 +0700

    create function authorizeRoles

---
+++ src/middlewares/authrole.js
@@ -0,0 +1,15 @@
+// middlewares/authorizeRoles.js
+
+const authorizeRoles = (...allowedRoles) => {
+  return (req, res, next) => {
+    const userRole = req.user?.role;
+
+    if (!userRole || !allowedRoles.includes(userRole)) {
+      return res.status(403).json({ message: "Không có quyền truy cập." });
+    }
+
+    next();
+  };
+};
+
+export default authorizeRoles;
