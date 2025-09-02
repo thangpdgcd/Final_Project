75d4084802f300013fa0eef204b5115920c385ae
Phan Đình Thắng <106921106+thangpdgcd@users.noreply.github.com>
2025-09-02 17:31:22 +0700

    create authenticator and connect frontend

--- src/server.js
+++ src/server.js
@@ -9,9 +9,13 @@ import initOrdersRoutes from "./routes/ordersRoutes.js";
 import initOrderItemsRoutes from "./routes/orderItemsRoutes.js"; // Import the order items routes
 import initCartRoutes from "./routes/cartsRoutes.js"; // Import the cart routes
 import initCartItemRoutes from "./routes/cartItemsRoutes.js"; // Import the cart items routes
-
+import authMiddleware from "../src/middlewares/auth.js";
+import initAuthenticated from "./routes/authRoutes.js";
+import initHomeRoutes from "./routes/homeRoutes.js"; // Import the home routes
 import { fileURLToPath } from "url";
 import path from "path";
+import cors from "cors";
+import bodyParser from "body-parser";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -21,7 +25,16 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(
+  cors({
+    origin: "http://localhost:5000", // FE đang chạy port 5000
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 // Cấu hình EJS
 configViewEngine(app);
 
@@ -34,13 +47,15 @@ initOrdersRoutes(app); // nếu có định tuyến đơn hàng
 initOrderItemsRoutes(app); // nếu có định tuyến đơn hàng
 initCartRoutes(app); // nếu có định tuyến giỏ hàng
 initCartItemRoutes(app);
-app; // nếu có định tuyến giỏ hàng
-app.get("/homeapi", (req, res) => {
-  res.render("homeapi"); // views/docs.ejs
-});
+initAuthenticated(app); // Khởi tạo các route đã xác thực
+initHomeRoutes(app); // Khởi tạo các route trang chủ
+// Route mẫu để kiểm tra middleware
 
+app.get("/homeapi", authMiddleware, (req, res) => {
+  res.json({ message: "Truy cập thành công", user: req.user });
+});
 app.use("/openapi.json", express.static(path.join(__dirname, "openapi.json")));
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}/homeapi`);
+  console.log(`Server is running at http://localhost:${PORT}/`);
 });
